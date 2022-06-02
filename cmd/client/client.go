c915260371a796f4707364ceff7f42db1e4e9f44
Vitor Porfirio <vhgporfirio@gmail.com>
2022-06-02 18:43:30 -0300

    feat: add streaming message for client and server

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/vitor-porfirio/fc3-grpc/pb"
 	"google.golang.org/grpc"
@@ -21,7 +23,9 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 
-	AddUser(client)
+	// AddUser(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -34,6 +38,80 @@ func AddUser(client pb.UserServiceClient) {
 	res, err := client.AddUser(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Vitor",
+		Email: "vhgporfirio@gmail.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the message: %v", err)
+		}
+
+		fmt.Println("Status:", stream.Status, "-", stream.GetUser())
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "v1",
+			Name:  "Vitor",
+			Email: "vitor@123.com",
+		},
+		&pb.User{
+			Id:    "v2",
+			Name:  "Vitor 2",
+			Email: "vitor2@123.com",
+		},
+		&pb.User{
+			Id:    "v3",
+			Name:  "Vitor 3",
+			Email: "vitor3@123.com",
+		},
+		&pb.User{
+			Id:    "v4",
+			Name:  "Vitor 4",
+			Email: "vitor4@123.com",
+		},
+		&pb.User{
+			Id:    "v5",
+			Name:  "Vitor 5",
+			Email: "vitor5@123.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		fmt.Println("Sending: ", req)
+		time.Sleep(time.Second * 2)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
 	}
 
 	fmt.Println(res)
--- services/user.go
+++ services/user.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/vitor-porfirio/fc3-grpc/pb"
@@ -62,8 +64,38 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 
 	stream.Send(&pb.UserResultStream{
 		Status: "Completed",
-		User:   &pb.User{},
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
 	})
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
+
 }
