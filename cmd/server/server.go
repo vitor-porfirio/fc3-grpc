341493ade20f0d347f4cfa66b3f431aba3324745
Vitor Porfirio <vhgporfirio@gmail.com>
2022-06-02 12:35:51 -0300

    feat: register service on server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net"
 
+	"github.com/vitor-porfirio/fc3-grpc/pb"
+	"github.com/vitor-porfirio/fc3-grpc/services"
 	"google.golang.org/grpc"
 )
 
@@ -15,6 +17,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
