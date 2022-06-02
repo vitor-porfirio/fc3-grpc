20d26113b7fc2c4bb79c0cbe90145fb1009ff806
Vitor Porfirio <vhgporfirio@gmail.com>
2022-06-02 12:35:18 -0300

    feat: create new service

---
+++ services/user.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/vitor-porfirio/fc3-grpc/pb"
+)
+
+// type UserServiceServer interface {
+// 	AddUser(context.Context, *User) (*User, error)
+// 	mustEmbedUnimplementedUserServiceServer()
+// }
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+
+	//Insert - Database
+	fmt.Println(req.Name)
+
+	return &pb.User{
+		Id:    "123",
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
+	}, nil
+}
