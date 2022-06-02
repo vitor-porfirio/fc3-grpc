c36d38128c45dbd165fbbca5682990aac59ad9ab
Vitor Porfirio <vhgporfirio@gmail.com>
2022-06-02 14:10:58 -0300

    feat: put server in reflection mode

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/vitor-porfirio/fc3-grpc/pb"
 	"github.com/vitor-porfirio/fc3-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
